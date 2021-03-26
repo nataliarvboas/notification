e9e6b0a957077a9fcb99ba8f4d2d437e5fee508a
nataliarvboas <nataliarvboas@gmail.com>
2021-03-26 10:10:24 -0300

    First notification example

---
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.notification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnSend: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.button_send_notification)
+        btnSend.setOnClickListener {
+            this.showNotification("1234", "Bootcamp Android", "Kotlin Android - Curso")
+        }
+    }
+
+
+}
